82058a389576f45a1389449f33b1906e9e74db44
computer <av8r.08@gmail.com>
2018-08-02 21:56:02 -0500

    worked out how to get the table loaded in. Still needs work on removing extrainius information (colums on the far right and such)

--- JS TMDE/ElectronTMDE/main.js
+++ JS TMDE/ElectronTMDE/main.js
@@ -1,6 +1,8 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
+const XLSX = require('xlsx');
+tableify = require('tableify');
 
 const {app, BrowserWindow, Menu, ipcMain} = electron;
 
@@ -28,7 +30,12 @@ Menu.setApplicationMenu(mainMenu);
 });
 
 // TODO: Load tmde tracker excel into a table
+var workbook = XLSX.readFile('TMDE EQUIPMENT.xlsm');
+var sheet_name_list = workbook.SheetNames;
+var sheet = sheet_name_list.indexOf('Consolidated');
 
+
+var consolodated = XLSX.utils.sheet_to_html(workbook.Sheets[sheet_name_list[1]], {id : "tracker"});
 // TODO: make tools to quickly update the tracker
 
 // TODO: in a perfect world scanned returned doc could auto update the tracker
@@ -40,9 +47,22 @@ Menu.setApplicationMenu(mainMenu);
 // create menu template
 const mainMenuTemplate = [
     {
-        label: 'File'
+        label: 'File',
+        submenu:[
+            {
+                label:'Clear Items',
+            click(){
+                sendTable();
+                }
+        }
+        ]
     }
 ];
+
+function sendTable(){
+    mainWindow.webContents.send('table:add', consolodated);
+    console.log('456');
+}
 
 // If Mac add empty object to menu
 if(process.platform == 'darwin'){
